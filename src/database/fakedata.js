14aea718bd0318b8277b8bd2d63e076402760064
carollinaam <carollourenco.design@gmail.com>
2020-10-15 22:05:11 -0300

    day4, configurando backend, aprendi mais sobre javascript e enteder separar  back e front

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-19.9266899",
+    lng: "-43.9751174",
+    name: "Lar das meninas",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1539893867126-7ce0b48971ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", 
+
+      "https://images.unsplash.com/photo-1534982841079-afde227ada8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    ], 
+    instructions: "Venha para se sentir à vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas: das 18h até 8h",
+    open_on_weekends: "1"
+  },
+  {
+    id: 2,
+    lat: "-19.8442484",
+    lng: "-43.986292517",
+    name: "Lar dos meninos",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1518101645466-7795885ff8f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", 
+
+      "https://images.unsplash.com/photo-1539893867126-7ce0b48971ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    ], 
+    instructions: "Venha para se sentir à vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas: das 6h até 18h",
+    open_on_weekends: "0",
+  }
+  
+]
